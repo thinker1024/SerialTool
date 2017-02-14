f07261758e036b4600beb15f020d1f91f1d45750
Tao Yang <yangtao.now@gmail.com>
2017-02-14 16:23:43 +0800

    add parentheses for print() to comply python3

--- SerialTool/SerialPy.py
+++ SerialTool/SerialPy.py
@@ -6,8 +6,8 @@ import serial
 import time
 import sys
 
-print "\r\nA small debug tool for serial port programming."
-print "-----------------\r\n"
+print ("\r\nA small debug tool for serial port programming.")
+print ("-----------------\r\n")
 # The serial port configuration
 def HexShow(argv):
     result = ''
@@ -20,22 +20,21 @@ def HexShow(argv):
 
 def main():
     if len(sys.argv) < 6:
-        print "usage:"
-        print "====\r\n"
-        print "SerialTool com baudrate databits parity stopbits [TX data type] [RX data type]"
-        print "|_com: the serial port device name"
-        print "|_baudrate: any standard baudrate, such as 9600, 115200, etc"
-        print "|_databits: 5,6,7,8"
-        print "|_parity: N,E"
-        print "|_stopbits: 1,1.5,2"
-        print "|_TX data types: string or hex, default is string if this parameter is null"
-        print "|_RX data types: string or hex, default is string if this parameter is null"
-        print "\r\nsimple usage example:"
-        print "====\r\n"
-        print "SerialTool /dev/ttyUSB0 115200 8 N 1 hex hex"
-        print "\r\nCONTACT"
-        print "====\r\n"
-        print "Project main page: https://pypi.python.org/pypi/SerialTool"
+        print ("usage:")
+        print ("SerialTool com baudrate databits parity stopbits [TX data type] [RX data type]")
+        print ("|_com: the serial port device name")
+        print ("|_baudrate: any standard baudrate, such as 9600, 115200, etc")
+        print ("|_databits: 5,6,7,8")
+        print ("|_parity: N,E")
+        print ("|_stopbits: 1,1.5,2")
+        print ("|_TX data types: string or hex, default is string if this parameter is null")
+        print ("|_RX data types: string or hex, default is string if this parameter is null")
+        print ("\r\nsimple usage example:")
+        print ("====\r\n")
+        print ("SerialTool /dev/ttyUSB0 115200 8 N 1 hex hex")
+        print ("\r\nCONTACT")
+        print ("====\r\n")
+        print ("Project main page: https://pypi.python.org/pypi/SerialTool")
         exit()
     ser = serial.Serial(
             port = sys.argv[1],
@@ -48,10 +47,10 @@ def main():
             rtscts = None,
             interCharTimeout = None
         )
-    print "Port Info:" + ser.portstr
-    print ">>:send"
-    print "<<:receive"
-    print "====\r\n"
+    print ("Port Info:" + ser.portstr)
+    print (">>:send")
+    print ("<<:receive")
+    print ("====\r\n")
     ser.isOpen()
 
     try:
@@ -63,11 +62,11 @@ def main():
             if RecvData != '':
                 if len(sys.argv) > 7 and sys.argv[7] == 'hex':
                     RecvData = HexShow(RecvData)
-                    print "<<" + RecvData
+                    print ("<<" + RecvData)
                 elif len(sys.argv) > 7 and sys.argv[7] == 'string':
-                    print "<<" + RecvData
+                    print ("<<" + RecvData)
                 else:
-                    print "<<" + RecvData
+                    print ("<<" + RecvData)
 
             InPut = raw_input(">>")
             if InPut == 'exit':
