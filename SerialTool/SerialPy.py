8ba312072f076acbfdf6b31b5bfdcb7b74c9bb1b
Tao Yang <yangtao.now@gmail.com>
2017-03-14 18:25:18 +0800

    add -s option to implement default port configurations

--- SerialTool/SerialPy.py
+++ SerialTool/SerialPy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 __author__ = "Tao Yang"
 
@@ -6,6 +7,7 @@ import serial
 import time
 import sys
 import readline
+import os
 
 print ("\r\nA small debug tool for serial port programming.")
 print ("-----------------\r\n")
@@ -19,36 +21,89 @@ def HexShow(argv):
         result += hhex+''
     return result
 
+def PrintHelp():
+    print ("usage:")
+    print ("SerialTool [-s] com baudrate databits parity stopbits [TX data type] [RX data type]")
+    print ("|_-s: save the current port configuration as default option")
+    print ("|_com: the serial port device name")
+    print ("|_baudrate: any standard baudrate, such as 9600, 115200, etc")
+    print ("|_databits: 5,6,7,8")
+    print ("|_parity: N,E")
+    print ("|_stopbits: 1,1.5,2")
+    print ("|_TX data types: string or hex, default is string if this parameter is null")
+    print ("|_RX data types: string or hex, default is string if this parameter is null")
+    print ("\r\nsimple usage example:")
+    print ("====\r\n")
+    print ("SerialTool /dev/ttyUSB0 115200 8 N 1 hex hex")
+    print ("\r\nCONTACT")
+    print ("====\r\n")
+    print ("Project main page: https://pypi.python.org/pypi/SerialTool")
+
+
 def main():
-    if len(sys.argv) < 6:
-        print ("usage:")
-        print ("SerialTool com baudrate databits parity stopbits [TX data type] [RX data type]")
-        print ("|_com: the serial port device name")
-        print ("|_baudrate: any standard baudrate, such as 9600, 115200, etc")
-        print ("|_databits: 5,6,7,8")
-        print ("|_parity: N,E")
-        print ("|_stopbits: 1,1.5,2")
-        print ("|_TX data types: string or hex, default is string if this parameter is null")
-        print ("|_RX data types: string or hex, default is string if this parameter is null")
-        print ("\r\nsimple usage example:")
-        print ("====\r\n")
-        print ("SerialTool /dev/ttyUSB0 115200 8 N 1 hex hex")
-        print ("\r\nCONTACT")
-        print ("====\r\n")
-        print ("Project main page: https://pypi.python.org/pypi/SerialTool")
+    ser = serial.Serial()
+    if len(sys.argv) < 6 and os.path.isfile('/.SerialTool.conf') == False:
+        PrintHelp()
         exit()
-    ser = serial.Serial(
-            port = sys.argv[1],
-            baudrate = sys.argv[2],
-            bytesize = int(sys.argv[3]),
-            parity = sys.argv[4],
-            stopbits = int(sys.argv[5]),
-            timeout = 0.5,
-            xonxoff = None,
-            rtscts = None,
-            interCharTimeout = None
-        )
-    print ("Port Info:" + ser.portstr)
+    elif len(sys.argv) > 1 and len(sys.argv) < 6 :
+        PrintHelp()
+        exit()
+    print ("Port Info:")
+    if os.path.isfile('/.SerialTool.conf') and len(sys.argv) == 1:
+        with open('/.SerialTool.conf', 'r') as fp:
+            x = fp.readlines()
+            param = []
+            for lines in x :
+                lines = lines.replace("\r\n","").split(",")
+                param.extend(lines)
+
+            ser = serial.Serial(
+                port = param[0],
+                baudrate = param[1],
+                bytesize = int(param[2]),
+                parity = param[3],
+                stopbits = int(param[4]),
+                timeout = 0.5,
+                xonxoff = None,
+                rtscts = None,
+                interCharTimeout = None
+                )
+            for i in param:
+                print (i)
+
+    elif len(sys.argv) > 6 and sys.argv[1] == '-s' :
+        config = sys.argv[2:9]
+        with open("/.SerialTool.conf", "w") as fp:
+            for i in config :
+                print (i)
+                fp.write(i)
+                fp.write('\r\n')
+            ser = serial.Serial(
+                    port = sys.argv[2],
+                    baudrate = sys.argv[3],
+                    bytesize = int(sys.argv[4]),
+                    parity = sys.argv[5],
+                    stopbits = int(sys.argv[6]),
+                    timeout = 0.5,
+                    xonxoff = None,
+                    rtscts = None,
+                    interCharTimeout = None
+                    )
+
+    elif len(sys.argv) == 6 or len(sys.argv) == 8 :
+        ser = serial.Serial(
+                port = sys.argv[1],
+                baudrate = sys.argv[2],
+                bytesize = int(sys.argv[3]),
+                parity = sys.argv[4],
+                stopbits = int(sys.argv[5]),
+                timeout = 0.5,
+                xonxoff = None,
+                rtscts = None,
+                interCharTimeout = None
+                )
+        for i in sys.argv[1:6] :
+            print (i)
     print (">>:send")
     print ("<<:receive")
     print ("====\r\n")
--- SerialTool/__init__.py
+++ SerialTool/__init__.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python
-__version__ = "0.1.0"
+__version__ = "0.1.2"
